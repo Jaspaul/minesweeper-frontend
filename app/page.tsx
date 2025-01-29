53619bce33ab28844df1af6b9c7e9c0a5364a2a6
Jaspaul Bola <jaspaul.b@gmail.com>
2025-01-29 01:05:56 -0500

    Add ability to create a new game

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+export default function Page() {
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  async function createGame(event: any) {
+    event.preventDefault();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/minesweeper/`,
+      {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          player_name: event.target.player_name.value,
+          rows: event.target.rows.value,
+          columns: event.target.columns.value,
+          bomb_count: event.target.bomb_count.value,
+        }),
+      },
+    );
+
+    const { id } = await res.json();
+    window.location.href = `/${id}`;
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <form className="w-full max-w-sm" onSubmit={createGame}>
+          <div className="mb-4">
+            <label>Player Name</label>
+            <input
+              type="text"
+              id="player_name"
+              className="rounded w-full py-2 px-3 text-black"
+              placeholder="Enter player name"
+              required
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          </div>
+
+          <div className="mb-4">
+            <label>Rows (2 - 20)</label>
+            <input
+              type="number"
+              id="rows"
+              min="2"
+              max="20"
+              className="rounded w-full py-2 px-3 text-black"
+              placeholder="Enter rows"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label>Columns (2 - 20)</label>
+            <input
+              type="number"
+              id="columns"
+              min="2"
+              max="20"
+              className="rounded w-full py-2 px-3 text-black"
+              placeholder="Enter columns"
+              required
+            />
+          </div>
+
+          <div className="mb-8">
+            <label>Bomb Count</label>
+            <input
+              type="number"
+              id="bomb_count"
+              className="rounded w-full py-2 px-3 text-black"
+              placeholder="Enter count"
+              required
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="rounded bg-blue-500 w-full font-bold py-2 px-4"
+            >
+              Create Game
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
